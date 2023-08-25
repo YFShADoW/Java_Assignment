f51f2e541a47ff41bad0c755e4ca1f95c18993a9
YF <chooiyaofeng@gmail.com>
2023-08-25 10:53:59 +0800

    File Function Done

--- src/purchaseordermanagementsystem/FileManager.java
+++ src/purchaseordermanagementsystem/FileManager.java
@@ -10,68 +10,32 @@ package purchaseordermanagementsystem;
  */
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import javax.swing.JOptionPane;
 
 public class FileManager {
-    ArrayList<User> users= new ArrayList<User>();
-//    public void saveToArrayLIst(String fileName){
-//        try{
-//            FileInputStream file = new FileInputStream(fileName);
-//            ObjectInputStream inputFile = new ObjectInputStream(file);
-//            
-//            boolean endOfFile = false;
-//            
-//            while (! endOfFile){
-//                try{
-//                    users.add((User) inputFile.readObject())
-//                }
-//                catch(EOFException e){
-//                    endOfFile = true;
-//                }
-//                catch(Exception f){
-//                JOptionPane.showMessageDialog(null,f.getMessage());                    
-//                }
-//            }
-//        }
-//        catch(IOException e){
-//            JOptionPane.showMessageDialog(null,e.getMessage());
-//        }
-//    }
+
     // Constructor
     private String fileName;
-
     public FileManager(String fileName) {
         this.fileName = fileName;
     }
-
-//    public void saveToArrayList(ArrayList<Administrator> objects) {
-//        ArrayList<String> lines = new ArrayList<>();
-//        for (User obj : objects) {
-//            lines.add(obj.getUserID());
-//            lines.add(obj.getUserName());
-//            lines.add(obj.getUserEmail());
-//            lines.add(obj.getUserPhone());
-//            lines.add(obj.getUserType());
-//            
-//        }
-//        writeFile(lines);
-//    }
-//    public String[] stringListToArray(ArrayList<String> objects){
-//        for(int i=0 ; i< objects.size();i++){
-//            String line = objects.get(i).toString();
-//            String[] data = line.split("\\|");
-//        }
-//    
-//    }
+    public ArrayList<String[]> saveTo2DArrayList(){
+        ArrayList<String[]> ArrayList2D = new ArrayList();
+        ArrayList<String> datalines = this.readFile();
+        for (String line:datalines){
+            String[] dataArray = line.trim().split("\\|");
+            ArrayList2D.add(dataArray);
+        }
+        return ArrayList2D;
+    }
     
     public void addToFile(String[] newObject){
         try{
             FileWriter file = new FileWriter(fileName,true);
             BufferedWriter writer = new BufferedWriter(file);
-            for(String data: newObject){
-                writer.append(data);
-                writer.append("|");
-            }
+            String line = String.join("|", newObject);
+            writer.append(line);
             writer.append("\n");
             writer.close();
             
@@ -80,25 +44,53 @@ public class FileManager {
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }
-    public void removeLineFromFile(String target){
-        ArrayList<String> fileData = this.readFile();
-        ArrayList<ArrayList<String>> listData = new ArrayList();
-        System.out.println(fileData);
-        
+
+    
+    public void removeLineFromFile(String primaryKey){
+        ArrayList<String[]> dataLines = this.saveTo2DArrayList();
+        try{
+            FileWriter file = new FileWriter(fileName);
+            BufferedWriter writer = new BufferedWriter(file);
+            for(String[] data:dataLines){
+                if(data[0].equals(primaryKey)){
+                    continue;
+                }
+                else{
+                    String line = String.join("|", data);
+                    writer.write(line+"\n");
+                    writer.flush();
+                }
+            }
+        }
+        catch(IOException e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
     }
     
     
-//    public void writeFile(ArrayList<String> lines) {
-//        try (FileWriter fileWriter = new FileWriter(fileName)) {
-//            for (String line : lines) {
-//                fileWriter.write(line);
-//                fileWriter.write(System.lineSeparator());
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        
-//    }
+    public void editFile(String[] targetLine, String[] newLine) {
+        ArrayList<String[]> dataLines = this.saveTo2DArrayList();
+        for(int i =0 ; i <dataLines.size();i++){
+            if(Arrays.equals(dataLines.get(i) ,targetLine)){
+                dataLines.set(i, newLine);
+                System.out.println("yes");
+            }
+        }
+        
+        try{
+            FileWriter file = new FileWriter(fileName);
+            BufferedWriter writer = new BufferedWriter(file);
+            for(String[] data:dataLines){
+                String line = String.join("|", data);
+                writer.write(line+"\n");
+                writer.flush();         
+            }
+            
+        }
+        catch(IOException e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }   
+    }
     
     public ArrayList<String> readFile() {
         ArrayList<String> object = new ArrayList<String>();
@@ -115,5 +107,14 @@ public class FileManager {
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
         return object;
-    }    
+    }
+//    public static void main(String[] args){
+//        FileManager file = new FileManager("User.txt");
+////        file.removeLineFromFile("U00001");
+//        String[] targetLine = "U00002|YF|YF0322|0189796991|YF@gmail.com|Admin|A00002".split("\\|");
+//        String[] newLine = "U00002|YF|YF0322|0189796991|YaoFeng@gmail.com|Admin|A00002".split("\\|");
+//        file.editFile(targetLine, newLine);
+//// U00001|admin01|admin1234|admin01@gmail.com|0123456789|Admin|A00001
+//// U00002|YF|YF0322|0189796991|YF@gmail.com|Admin|A00002
+//    }
 }
